b477e627de5a04b0dbf89fc4ff49609d0cab3226
KevinF42 <kforrister@segfault.com>
2017-03-15 08:35:01 -0700

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Foxtrot.ioe</title>
+  <!--  No href external links to css or js-->
+   <link rel="stylesheet" href="css/custom.css">
+	    <style>
+			#if-location {
+			position: absolute; left: 10px; top:200px;
+			width:800px;
+			height:500px;
+		}
+</style>
+
+</head>
+<body>
+<!--  add a menu here -->
+<a href="phpcode/" class= "menu" >phpcode</a>&nbsp;
+<a href="downloads/" class= "slink" >downloads</a>&nbsp;
+<a href="qnb/" class= "menu" >pma</a>&nbsp;
+<a href="logs/" class= "slink" target = "_blank" >logs</a>
+<a href="contact.html" class= "menu" target = "if">Contact</a>
+<a href="intro.html" class= "slink" target = "if" >Contact</a>
+<a href="http://10.183.1.6:8080" class = "menu1" >nodeserver:8080</a>
+<a href="http://10.183.1.6/html/" class= "menu" target = "if">html</a>
+<a href = "https://sites.google.com/a/tritechsc.org/coleman/" target = "_blank"><img src = "img/google.png"></a>
+
+<iframe  id="if-location" name="if" src="intro.html"  scrolling="yes" frameborder="3"></iframe>
+
+</body>
+</html>
